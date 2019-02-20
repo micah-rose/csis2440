d49fef09f9e9d3471bf25253b323cdd150fd613b
Micah Lund <micah.r.lund@gmail.com>
2019-02-20 15:41:51 -0700

    in class notes for code modifications

--- palindrome/index.php
+++ palindrome/index.php
@@ -48,14 +48,32 @@ RegEx with PHP Example:
 
 The below throws an error as of 02/12:
 <?php 
-    $phone = "123-456-7890";
-    $pattern = "\d{3}-\d{3}-\d{4}";
+    //$phone = "123-456-7890";
+    //$pattern = "\d{3}-\d{3}-\d{4}";
 
-    if(preg_match($pattern, $phone)){
-        echo "Yay! It matches!!";
-    } else {
-        echo "Boo!!";
-    }
+    //if(preg_match($pattern, $phone)){
+        //echo "Yay! It matches!!";
+    //} else {
+        //echo "Boo!!";
+    //}
+
+    #Add to top (above DOCTYPE)
+    //if (isset($_GET['w'])) $wValue = $_GET['w']; else $wValue = "";
+    //if (isset($_GET['pc'])) $pcValue = $_GET['pc']; else $wValue = 0;
+    //if (isset($_GET['ec'])) $ecValue = $_GET['ec']; else $ecValue = 0;
+
+    #Add within option tag and inner PHP
+    //$pcValue =
+    //if (!$pcValue || $pcValue > $maxPalis) echo "selected"; #end php
+    //if ($pcValue == $x) 
+    //echo'<option selected value="' .$x. '"> ' .$x. ' ' .$output. '</option>'
+    //else echo '<option value="' .$x. '"> ' .$x. ' ' .$output. '</option>'
+
+    //switch($ecValue){ 
+        //case 3: echo '<p class ="warning"> Please enter a search term.</p>'; 
+        //case 2: echo '<p class ="warning"> Please select a number of palindromes.</p>'; break;
+        //case 1: echo '<p class ="warning"> Please enter a search term.</p>'; break;
+        //default:}
 ?>
 
 -->
