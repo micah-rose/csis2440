ebb49a00a662540b69043314edcfcd8d2d27b77e
Micah Lund <micah.r.lund@gmail.com>
2019-03-03 23:35:37 -0700

    updated with validation assigment

---
+++ validation/index.php
@@ -0,0 +1,60 @@
+<?php
+
+echo getVariables();
+
+function getVariables(){
+    global $pValue;
+    global $eValue;
+    global $ecValue;
+
+    if (isset($_GET["v"]))
+        $displayError = true;
+    else   
+        $displayError = false;
+
+    if (isset($_GET['p'])) $pValue = $_GET['p']; else $pValue = "";
+    if (isset($_GET['e'])) $eValue = $_GET['e']; else $pValue = "";
+    if (isset($_GET['ec'])) $ecValue = $_GET['ec']; else $ecValue = 0;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Validation</title>
+        <link type="text/css" rel="stylesheet" href="/validation/css/validation.css" />
+
+    </head>
+    <body>
+    <div class="main-wrapper">
+    <h2>Let's Validate Yo' Info</h2>
+    <div class="form-wrapper">
+<form method="post" action="process.php">
+    <input name='phone' type='text' placeholder='Phone Number' value= '<?php echo $pValue; ?>'>
+    <input name= 'email' type='text' placeholder='Email Address' value= '<?php echo $eValue; ?>'>
+    <input type="submit">
+    <input type="reset">
+</form>
+<?php
+
+    echo displayError($ecValue);
+
+
+function displayError($ec){
+    $returnVal = "";
+
+    switch($ec){ 
+        case 3: $returnVal .= '<p class ="warning"> Please enter a valid phone number.</p>'; 
+        case 2: $returnVal .= '<p class ="warning"> Please enter a valid email address.</p>'; break;
+        case 1: $returnVal .= '<p class ="warning">Please enter a valid phone number.</p>'; break;
+    }
+    return $returnVal;
+}
+
+?>
+
+
+</div>
+</div>
+    </body>
+</html>
---
+++ validation/process.php
@@ -0,0 +1,49 @@
+<?php 
+    $phone = "";
+    $email = "";
+
+    $pPattern = "/\(\d{3}\)\d{3}-\d{4}/"; 
+    $ec = 0;
+
+    if (!empty($_POST["phone"])){
+        $phone = $_POST["phone"];
+    } else $ec += 1;
+    if (!empty($_POST["email"])){ //for some reason this code isn't running
+        $email = $_POST["email"];
+    } else $ec += 2;
+
+    if (preg_match($pPattern, $phone) && filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $hooray = "<p>Thank you for submitting valid info!! Both " . $phone . " and " . $email . " were valid!!</p>";
+    }
+    if (!preg_match($pPattern, $phone) && filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $ec += 1;
+    } 
+    if (preg_match($pPattern, $phone) && !filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $ec += 2;
+    }
+    if (!preg_match($pPattern, $phone) && !filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $ec += 3;
+    }
+    if ($ec){
+        header("location: index.php?p= " .$phone . "&e=" . $email . "&ec=" .$ec);
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Validation</title>
+        <link type="text/css" rel="stylesheet" href="/validation/css/validation.css" />
+
+    </head>
+    <body>
+    <div class="main-wrapper">
+    <h2>You's been VALIDATED!!</h2>
+<?php
+
+    echo $hooray;
+
+?>
+</div>
+    </body>
+</html>
