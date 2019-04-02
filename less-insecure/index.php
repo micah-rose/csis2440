449ef658639090bb392cb586cbf51f2e32c4295c
Micah Lund <micah.r.lund@gmail.com>
2019-04-01 21:17:13 -0600

    determined my database connection issue was actually me trying to connect PHP from my local machine to a life server. Was able to successfully complete the code for that assignment by loading it onto the server and checking functionality from there.

--- less-insecure/index.php
+++ less-insecure/index.php
@@ -1,54 +1,31 @@
 <?php 
 
-//define("HOST", "127.0.0.1");
-//define("USER", "users");
-//define("PASS", "stillsucks");
-//define("BASE", "insecure");
-
-//$conn = mysqli_connect(HOST, USER, PASS, BASE);
-
-$host = "127.0.0.1";
-$user = "users";
-$password = "stillsucks";
-$dbName = "insecure";
-
-//$conn = mysqli_connect($host, $user, $password, $dbName);
-
-try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbName", $user, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
-    } catch(PDOException $e) {    
-    echo "Connection failed: " . $e->getMessage();
-    }
-
-/*
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-} else echo "Connected successfully"; */
-
-//$sql = "SELECT * FROM user_table;";
-//$results = mysqli_query($conn, $sql);
-
-if(isset($_POST['submit'])){
-
-
-/*
     $access = '';
-    if(in_array(strtolower($_POST['user']), $userArray) && in_array(strtolower($_POST['password']), $pwArray)){
-        if(strtolower($_POST['user']) == $userArray[1] && strtolower($_POST['password']) == $pwArray[2] 
-        || strtolower($_POST['user']) == $userArray[3] && strtolower($_POST['password']) == $pwArray[4] 
-        || strtolower($_POST['user']) == $userArray[5] && strtolower($_POST['password']) == $pwArray[6] 
-        || strtolower($_POST['user']) == $userArray[7] && strtolower($_POST['password']) == $pwArray[8]){
 
+    define("HOST", "localhost");
+    define("USER", "id8433217_users");
+    define("PASS", "stillsucks");
+    define("BASE", "id8433217_insecure");
+
+    $conn = mysqli_connect(HOST, USER, PASS, BASE);
+
+    if(isset($_POST['submit'])){
+
+        $name = $_POST["user"]; 
+        $password = $_POST["password"]; 
+
+        $sql = "SELECT Username, Password FROM user_table WHERE Username = '".$name."' AND  Password = '".$password."';";
+        $result = mysqli_query($conn, $sql);
+
+        if(mysqli_num_rows($result) > 0 ){ 
             $access = '<p>Access Granted</p>';
-        } else $access = '<p>Access Denied</p>';
-    } else $access = '<p>Access Denied</p>';
-*/
-
-}
+        }
+        else{
+            $access = '<p>Access Denied</p>';
+        }
+    }   
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -57,9 +34,6 @@ if(isset($_POST['submit'])){
     </head>
     <body>
         <?php 
-            ob_start();
-            include ('includes/users.txt'); 
-            $data = ob_get_clean();
             echo $access; 
         ?>
         <form method="post">
