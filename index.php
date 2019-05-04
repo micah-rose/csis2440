eed01fbf0f8903bc46beb9c587fdb57147f7654b
Micah Lund <micah.r.lund@gmail.com>
2019-05-04 09:43:56 -0600

    adjusted more of the formatting so that everything is on one page. Also added IDs to the nav links so that when clicked the page directs to that section.

--- index.php
+++ index.php
@@ -19,10 +19,11 @@
 
   <!-- Custom styles for this template -->
   <link href="css/business-casual.min.css" rel="stylesheet">
+  <script src="js/index.js"></script>
 
 </head>
 
-<body>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
 
   <h1 class="site-heading text-center text-white d-none d-lg-block">
     <span class="site-heading-upper text-primary mb-3">A Free Bootstrap 4 Business Theme</span>
@@ -33,26 +34,35 @@
   <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
     <div class="container">
       <a class="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none" href="#">Start Bootstrap</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <button 
+        class="navbar-toggler" 
+        type="button" 
+        data-toggle="collapse" 
+        data-target="#navbarResponsive" >
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav mx-auto">
           <li class="nav-item active px-lg-4">
-            <a class="nav-link text-uppercase text-expanded" href="index.php">Home
+            <a class="nav-link text-uppercase text-expanded" href="#home">Home
               <span class="sr-only">(current)</span>
             </a>
           </li>
           <li class="nav-item px-lg-4">
-            <a class="nav-link text-uppercase text-expanded" href="courseworks.html">Courseworks</a>
+            <a class="nav-link text-uppercase text-expanded" href="#about"
+                >About Me</a
+              >
           </li>
           <li class="nav-item px-lg-4">
-            <a class="nav-link text-uppercase text-expanded" href="reflection.html"
+            <a class="nav-link text-uppercase text-expanded" href="#reflection"
                 >Reflection</a
               >
           </li>
           <li class="nav-item px-lg-4">
-            <a class="nav-link text-uppercase text-expanded" href="csis1430.html">1430 Site</a>
+            <a class="nav-link text-uppercase text-expanded" href="#courseworks">Courseworks</a>
+          </li>
+          <li class="nav-item px-lg-4">
+            <a class="nav-link text-uppercase text-expanded" href="#eportfolio">e-Portfolio & 1430 Site</a>
           </li>
         </ul>
       </div>
@@ -61,6 +71,16 @@
 
   <section class="page-section clearfix">
     <div class="container">
+
+    <form class="form-inline">
+    <div class="input-group">
+      <input type="text" name="name" class="form-control" size="50" placeholder="Enter Your Name" required>
+      <div class="input-group-btn">
+        <button type="submit" name="submit" class="btn btn-danger">Register</button>
+      </div>
+    </div>
+  </form>
+
       <div class="intro">
         <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="img/intro.jpg" alt="">
         <div class="intro-text left-0 text-center bg-faded p-5 rounded">
@@ -79,13 +99,13 @@
   </section>
 
   <section class="page-section cta">
-    <div class="container">
+    <div id="about" class="container-fluid">
       <div class="row">
         <div class="col-xl-9 mx-auto">
           <div class="cta-inner text-center rounded">
             <h2 class="section-heading mb-4">
-              <span class="section-heading-upper">Our Promise</span>
-              <span class="section-heading-lower">To You</span>
+              
+              <span class="section-heading-lower">About Me</span>
             </h2>
             <p class="mb-0">When you walk into our shop to start your day, we are dedicated to providing you with friendly service, a welcoming atmosphere, and above all else, excellent products made with the highest quality ingredients. If you are not satisfied, please let us know and we will do whatever we can to make things right!</p>
           </div>
@@ -94,9 +114,191 @@
     </div>
   </section>
 
+  <section class="page-section about-heading">
+      <div id="reflection" class="container-fluid">
+        <img
+          class="img-fluid rounded about-heading-img mb-3 mb-lg-0"
+          src="img/about.jpg"
+          alt=""
+        />
+        <div class="about-heading-content">
+          <div class="row">
+            <div class="col-xl-9 col-lg-10 mx-auto">
+              <div class="bg-faded rounded p-5">
+                <h2 class="section-heading mb-4">
+                  <span class="section-heading-lower">Reflection</span>
+                </h2>
+                <p>
+                  Founded in 1987 by the Hernandez brothers, our establishment
+                  has been serving up rich coffee sourced from artisan farmers
+                  in various regions of South and Central America. We are
+                  dedicated to travelling the world, finding the best coffee,
+                  and bringing back to you here in our cafe.
+                </p>
+                <p class="mb-0">
+                  We guarantee that you will fall in
+                  <em>lust</em>
+                  with our decadent blends the moment you walk inside until you
+                  finish your last sip. Join us for your daily routine, an
+                  outing with friends, or simply just to enjoy some alone time.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="page-section cta">
+    <div id="courseworks" class="container-fluid">
+      <div class="row">
+        <div class="col-xl-9 mx-auto">
+          <div class="cta-inner text-center rounded">
+            <h2 class="section-heading mb-4">
+              
+              <span class="section-heading-lower">Courseworks</span>
+            </h2>
+            <div class="container">
+                <div class="product-item">
+                    <div class="product-item-title d-flex">
+                        <div class="bg-faded p-5 d-flex ml-auto rounded">
+                            <h2 class="section-heading mb-0">
+                                <span class="section-heading-upper">Blended to Perfection</span>
+                                <span class="section-heading-lower">Coffees &amp; Teas</span>
+                            </h2>
+                        </div>
+                    </div>
+                    <img
+                        class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0"
+                        src="img/products-01.jpg"
+                        alt=""
+                    />
+                <div class="product-item-description d-flex mr-auto">
+                    <div class="bg-faded p-5 rounded">
+                        <p class="mb-0">
+                            We take pride in our work, and it shows. Every time you order a
+                            beverage from us, we guarantee that it will be an experience
+                            worth having. Whether it's our world famous Venezuelan
+                            Cappuccino, a refreshing iced herbal tea, or something as simple
+                            as a cup of speciality sourced black coffee, you will be coming
+                            back for more.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container">
+        <div class="product-item">
+          <div class="product-item-title d-flex">
+            <div class="bg-faded p-5 d-flex mr-auto rounded">
+              <h2 class="section-heading mb-0">
+                <span class="section-heading-upper"
+                  >Delicious Treats, Good Eats</span
+                >
+                <span class="section-heading-lower">Bakery &amp; Kitchen</span>
+              </h2>
+            </div>
+          </div>
+          <img
+            class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0"
+            src="img/products-02.jpg"
+            alt=""
+          />
+          <div class="product-item-description d-flex ml-auto">
+            <div class="bg-faded p-5 rounded">
+              <p class="mb-0">
+                Our seasonal menu features delicious snacks, baked goods, and
+                even full meals perfect for breakfast or lunchtime. We source
+                our ingredients from local, oragnic farms whenever possible,
+                alongside premium vendors for specialty goods.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="product-item">
+          <div class="product-item-title d-flex">
+            <div class="bg-faded p-5 d-flex ml-auto rounded">
+              <h2 class="section-heading mb-0">
+                <span class="section-heading-upper">From Around the World</span>
+                <span class="section-heading-lower"
+                  >Bulk Speciality Blends</span
+                >
+              </h2>
+            </div>
+          </div>
+          <img
+            class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0"
+            src="img/products-03.jpg"
+            alt=""
+          />
+          <div class="product-item-description d-flex mr-auto">
+            <div class="bg-faded p-5 rounded">
+              <p class="mb-0">
+                Travelling the world for the very best quality coffee is
+                something take pride in. When you visit us, you'll always find
+                new blends from around the world, mainly from regions in Central
+                and South America. We sell our blends in smaller to large bulk
+                quantities. Please visit us in person for more details.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+        </div>
+      </div>
+    </div>
+    </div>
+    </div>
+  </section>
+
+  <section class="page-section about-heading">
+      <div id="eportfolio" class="container-fluid">
+        <img
+          class="img-fluid rounded about-heading-img mb-3 mb-lg-0"
+          src="img/about.jpg"
+          alt=""
+        />
+        <div class="about-heading-content">
+          <div class="row">
+            <div class="col-xl-9 col-lg-10 mx-auto">
+              <div class="bg-faded rounded p-5">
+                <h2 class="section-heading mb-4">
+                  <span class="section-heading-lower">e-Portfolio & 1430 Site</span>
+                </h2>
+                <p>
+                  Founded in 1987 by the Hernandez brothers, our establishment
+                  has been serving up rich coffee sourced from artisan farmers
+                  in various regions of South and Central America. We are
+                  dedicated to travelling the world, finding the best coffee,
+                  and bringing back to you here in our cafe.
+                </p>
+                <p class="mb-0">
+                  We guarantee that you will fall in
+                  <em>lust</em>
+                  with our decadent blends the moment you walk inside until you
+                  finish your last sip. Join us for your daily routine, an
+                  outing with friends, or simply just to enjoy some alone time.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
   <footer class="footer text-faded text-center py-5">
-    <div class="container">
-      <p class="m-0 small">Copyright &copy; Your Website 2019</p>
+    <div class="container-fluid">
+    <a href="#myPage" title="To Top">
+        <span class="glyphicon glyphicon-chevron-up"></span>
+      </a>
+      <p>
+        Thank you for visiting my site!!
+      </p>
+      <p class="m-0 small">Copyright &copy; Gojisaurus 2019</p>    
     </div>
   </footer>
 
