152bc546cb4db995a33169bc6971a7e64fa2584a
Micah Lund <micah.r.lund@gmail.com>
2020-08-07 09:07:46 -0600

    Figured out that Heroku has 'vendor' as a reserved word. So my folder named vendor was seen as forbidden. Changed the name of the folder and the errors on the console should go away now.

--- index.php
+++ index.php
@@ -384,8 +384,8 @@
   </footer>
 
   <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="styles/jquery/jquery.min.js"></script>
+  <script src="styles/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="js/index.js"></script>
 </body>
 
